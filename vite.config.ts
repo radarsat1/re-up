db3a9effbcb14cb7c9677582031893e5b9877d21
Stephen Sinclair <radarsat1@gmail.com>
2025-10-30 12:48:31 +0100

    Fix base URL.

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Set the base path for deployment to a subdirectory on GitHub Pages.
-  base: '/re-up-ai/', 
+  base: '/re-up/', 
 })
